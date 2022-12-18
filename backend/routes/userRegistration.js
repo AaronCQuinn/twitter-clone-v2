d8f246da3aeb4dbdca68f3c5f67a4e91038815d6
Aaron Quinn <aaron.quinn.nfld@gmail.com>
2022-12-18 20:14:32 -0330

    code won today, bugs in registration

--- backend/routes/userRegistration.js
+++ backend/routes/userRegistration.js
@@ -8,80 +8,75 @@ const bcrypt = require('bcrypt');
 const encryptPassword = require('../util/encryptPassword');
 
 router.post('/', async (req, res, next) => {
-    const { formValues } = req.body;
+    console.log(Object.keys(req.body));
+    // const { formValues } = req.body;
 
-    // Remove any spacing around post values.
-    const trimValues = {};
-    Object.keys(formValues).forEach(key => {
-        trimValues[key] = formValues[key].trim();
-    });
+    // // Remove any spacing around post values.
+    // const trimValues = {};
+    // Object.keys(formValues).forEach(key => {
+    //     trimValues[key] = formValues[key].trim();
+    // });
 
-    // Check to see all inputs have values.
-    if (!Object.values(trimValues).every(Boolean)) {
-        return res.status(400).send({
-            status: true,
-            text: "All fields must be submitted with a valid value in order to register."
-        })
-    }
+    // // Check to see all inputs have values.
+    // if (!Object.values(trimValues).every(Boolean)) {
+    //     return res.status(400).send({
+    //         text: "All fields must be submitted with a valid value in order to register."
+    //     })
+    // }
 
-    let firstName = req.body.formValues.regFirstName.trim();
-    let lastName = req.body.formValues.regLastName.trim();
-    let username = req.body.formValues.regUsername.trim();
-    let email = req.body.formValues.regEmail.trim();
-    let password = req.body.formValues.registerPassword;
-    let passwordConf = req.body.formValues.registerPasswordConf;
+    // let firstName = req.body.formValues.regFirstName.trim();
+    // let lastName = req.body.formValues.regLastName.trim();
+    // let username = req.body.formValues.regUsername.trim();
+    // let email = req.body.formValues.regEmail.trim();
+    // let password = req.body.formValues.registerPassword;
+    // let passwordConf = req.body.formValues.registerPasswordConf;
 
 
-    // Both passwords must pass validation checks.
-    if (!checkPassword(password) || !checkPassword(passwordConf)) {
-        return res.status(400).send({
-            status: true,
-            text: "Both passwords must be a minimum of 8 characters with an upper and lowercase character, as well as a number and symbol"
-        })
-    }
+    // // Both passwords must pass validation checks.
+    // if (!checkPassword(password) || !checkPassword(passwordConf)) {
+    //     return res.status(400).send({
+    //         text: "Both passwords must be a minimum of 8 characters with an upper and lowercase character, as well as a number and symbol"
+    //     })
+    // }
 
-    // Both passwords must match.
-    if (password !== passwordConf) {
-        return res.status(400).send({
-            status: true,
-            text: "Both passwords must match in order to register."
-        })
-    }
+    // // Both passwords must match.
+    // if (password !== passwordConf) {
+    //     return res.status(400).send({
+    //         text: "Both passwords must match in order to register."
+    //     })
+    // }
 
-    // Check if either the username or email already exists.
-    if (await User.findOne({$or: [{ username: username }, { email: email }]}).catch(error => {
-        console.log(error);
-        return res.status(500).send({
-            status: true,
-            text: "There was a server error. Please try again."
-        })}))
-     {
-        return res.status(400).send({
-            status: true,
-            text: "There is already a user registered under this username or email."
-        })
-    }
-   
-    User.create({
-        firstName,
-        lastName,
-        username,
-        email,
-        password: await encryptPassword(password),
-    }).
-    then(() => {
-        return res.status(200).send({
-            status: true,
-            text: "User successfully registered."
-        })}
-    )
-    .catch(err => {
-        console.log("Error writing user to the database: " + err);
-        return res.status(500).send({
-            status: true,
-            text: "There was a server error. Please try again."
-        })
-    })
+    // // Check if either the username or email already exists.
+    // const existingUser = await User.findOne({$or: [{ username: username }, { email: email }]}).catch(() => {
+    //     return res.status(500).send({
+    //         text: "There was a server error. Please try again."
+    //     });
+    // })
+
+    // if (existingUser) {
+    //     return res.status(409).json({
+    //         text: "There is already a user registered under this username or email."
+    //     });
+    // }
+    
+    // User.create({
+    //     firstName,
+    //     lastName,
+    //     username,
+    //     email,
+    //     password: await encryptPassword(password),
+    // }).
+    // then(() => {
+    //     return res.status(201).send({
+    //         text: "User successfully registered."
+    //     })}
+    // )
+    // .catch(err => {
+    //     console.log("Error writing user to the database: " + err);
+    //     return res.status(500).send({
+    //         text: "There was a server error. Please try again."
+    //     })
+    // })
 })
 
 module.exports = router;
