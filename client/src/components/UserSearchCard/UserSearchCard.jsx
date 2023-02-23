6271282455b5937991878b49a905fa01423e6b4a
Aaron Quinn <aaron.quinn.nfld@gmail.com>
2023-02-23 10:14:49 -0330

    ui changes

--- client/src/components/UserSearchCard/UserSearchCard.jsx
+++ client/src/components/UserSearchCard/UserSearchCard.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import './usersearchcard.css'
-import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const UserSearchCard = ({firstName, lastName, username}) => {
     return (
-        <div className="userSearchCard">
-            <FontAwesomeIcon icon={faXmark} size='xs' className='userSearchCardClose'/>
-            <div className=''>{firstName + " " + lastName}</div>
-            <span className='userSearchCard'>{"@" + username}</span>
-        </div>
+      <div className="userSearchCard">
+        <FontAwesomeIcon icon={faTimes} size="xs" className="userSearchCardClose" />
+        <div className="userSearchCardName">{firstName + " " + lastName}</div>
+        <div className="userSearchCardUsername">{"@" + username}</div>
+      </div>
     )
-}
+  }
+  
 
 export default UserSearchCard
