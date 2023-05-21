94e37bed24754cd895c757dc32d944dbe0d75317
Aaron Quinn <aaron.quinn.nfld@gmail.com>
2023-05-21 20:29:14 -0230

    more private message refactoring

---
+++ client/src/components/ChatMessages/ChatMessages.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react'
+import ChatBubble from '../../components/ChatBubble/ChatBubble';
+import { useRef } from 'react';
+
+const ChatMessages = ({ messageArray }) => {
+    const chatMessagesRef = useRef(null);
+
+    useEffect(() => {
+        chatMessagesRef.current.scrollTop = chatMessagesRef.current.scrollHeight;
+    }, [messageArray])
+    
+    return (
+        <ul className="chatMessages" ref={chatMessagesRef}>
+            {messageArray &&
+                messageArray.map((message, index) => {
+                    // First and last message, as if they're in a row, non-interupted by another user.
+                    let firstMessageFromUser = false;
+                    let lastMessageFromUser = false;
+                    const { _id: userWhoSentMessage } = message.userSent;
+                    const { _id: nextMessageUser } = index < messageArray.length - 1 && messageArray[index + 1]?.userSent;
+                    const { _id: previousMessageUser } = index > 0 && messageArray[index - 1]?.userSent;
+
+                    // The first message in the array always is the first.
+                    if (index === 0) {
+                        firstMessageFromUser = true;
+                    }
+
+                    // The last message in the array always is last.
+                    if (index === messageArray.length - 1) {
+                        lastMessageFromUser = true;
+                    }
+
+                    // If the message previous to the current isn't the same user, it must be the first.
+                    if (userWhoSentMessage !== previousMessageUser) {
+                        firstMessageFromUser = true;
+                    }
+
+                    // If the next message to the current isn't the same user, it must be the last.
+                    if (userWhoSentMessage !== nextMessageUser) {
+                        lastMessageFromUser = true;
+                    }
+
+                    return (
+                        <ChatBubble 
+                            message={message} 
+                            key={index} 
+                            index={index} 
+                            isFirstMessage={firstMessageFromUser} 
+                            isLastMessage={lastMessageFromUser}
+                        /> 
+                    )
+                })
+            }
+        </ul>
+    )
+}
+
+export default ChatMessages
--- client/src/pages/Chat/Chat.jsx
+++ client/src/pages/Chat/Chat.jsx
@@ -1,16 +1,18 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Container, Row, Col } from 'react-bootstrap/';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+
 import Spinner from '../../components/Spinner/Spinner'
 import Navbar from '../../components/Navbar/Navbar';
 import PageHeader from '../../components/PageHeader/PageHeader'
-import { useParams } from 'react-router-dom';
+import ChatHeader from '../../components/ChatHeader/ChatHeader';
+import ChatMessages from '../../components/ChatMessages/ChatMessages';
 import { showToast } from '../../components/Toast/showToast';
+
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import axios from 'axios';
 import './chat.css'
-import ChatHeader from '../../components/ChatHeader/ChatHeader';
-import ChatBubble from '../../components/ChatBubble/ChatBubble';
 
 const Chat = () => {
     const [loading, setLoading] = useState(true);
@@ -19,7 +21,6 @@ const Chat = () => {
     const [messageArray, setMessageArray] = useState();
     const [chat, setChat] = useState([]);
     const [disabled, setDisabled] = useState(false);
-    const chatMessagesRef = useRef(null);
     const params = useParams();
 
     useEffect(() => {
@@ -29,24 +30,18 @@ const Chat = () => {
 
     const getChatInfo = async() => {
         setLoading(true);
+
         try {
             const [chatInfoResponse, messagesResponse] = await Promise.all([
                 fetch(`/api/chats/${params.chatId}`),
                 fetch(`/api/messages/${params.chatId}`)
             ]);
+
             if (chatInfoResponse.ok && messagesResponse.ok) {
                 const chatInfo = await chatInfoResponse.json();
                 const messages = await messagesResponse.json();
                 setChat(chatInfo);
                 setMessageArray(messages);
-
-                if (chatMessagesRef.current) {
-                    chatMessagesRef.current.classList.add('scrolling');
-                    chatMessagesRef.current.scrollTop = chatMessagesRef.current.scrollHeight;
-                    chatMessagesRef.current.addEventListener('transitionend', () => {
-                      chatMessagesRef.current.classList.remove('scrolling');
-                    });
-                }
 
             } else if (chatInfoResponse.status === 401 || messagesResponse.status === 401) {
                 setChatError(true);
@@ -57,6 +52,7 @@ const Chat = () => {
         } finally {
             setLoading(false);
         }
+
     }
     
     const handleChange = (e) => {
@@ -108,65 +104,26 @@ const Chat = () => {
                 </Col>
 
                 <Col className="col-10 col-md-8 col-lg-6 h-100 flex">
-                {loading ? (
-                    <Spinner />
-                    ) : (
-                    chatError ? (
+                {loading ? <Spinner /> : 
+                    chatError ?
                         <div className="chatUnauthorized">
                             <div>The chat you tried to access either does not exist, or you are not a part of.</div>
                             <div>Please try again.</div>
                         </div>
-                    ) : (
+                    :
                     <div className="chatPageContainer" >
+
                         <PageHeader title={"Chat"} />
+
                         <div className="chatTitleBarContainer">
                             <ChatHeader fetchedChat={chat} />
                         </div>
-                        <div className="mainContentContainer" >
+
+                        <div className="mainContentContainer">
+                            
                             <div className="chatContainer">
 
-                                <ul className="chatMessages" ref={chatMessagesRef}>
-                                    {messageArray &&
-                                        messageArray.map((message, index) => {
-                                            // First and last message, as if they're in a row, non-interupted by another user.
-                                            let firstMessageFromUser = false;
-                                            let lastMessageFromUser = false;
-                                            const { _id: userWhoSentMessage } = message.userSent;
-                                            const { _id: nextMessageUser } = index < messageArray.length - 1 && messageArray[index + 1]?.userSent;
-                                            const { _id: previousMessageUser } = index > 0 && messageArray[index - 1]?.userSent;
-
-                                            // The first message in the array always is the first.
-                                            if (index === 0) {
-                                                firstMessageFromUser = true;
-                                            }
-
-                                            // The last message in the array always is last.
-                                            if (index === messageArray.length - 1) {
-                                                lastMessageFromUser = true;
-                                            }
-
-                                            // If the message previous to the current isn't the same user, it must be the first.
-                                            if (userWhoSentMessage !== previousMessageUser) {
-                                                firstMessageFromUser = true;
-                                            }
-
-                                            // If the next message to the current isn't the same user, it must be the last.
-                                            if (userWhoSentMessage !== nextMessageUser) {
-                                                lastMessageFromUser = true;
-                                            }
-
-                                            return (
-                                                <ChatBubble 
-                                                    message={message} 
-                                                    key={index} 
-                                                    index={index} 
-                                                    isFirstMessage={firstMessageFromUser} 
-                                                    isLastMessage={lastMessageFromUser}
-                                                /> 
-                                            )
-                                        })
-                                    }
-                                </ul>
+                                <ChatMessages messageArray={messageArray}/>
 
                                 <form method="post" onSubmit={e => handleSubmit(e)}>
                                     <div className="footer" >
@@ -176,11 +133,11 @@ const Chat = () => {
                                         </button>
                                     </div>
                                 </form>
+
                             </div>
                         </div>
                     </div>
-                    )
-                    )}
+                    }
 
                 </Col>
 
