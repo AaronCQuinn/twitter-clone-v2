6cf36acd3f58f83d9d83cf2baf4f717b2fccb069
Aaron Quinn <aaron.quinn.nfld@gmail.com>
2023-05-21 10:28:54 -0230

    refactored of dm msg conditional rendering of classes, much cleaner

--- client/src/components/ChatBubble/ChatBubble.jsx
+++ client/src/components/ChatBubble/ChatBubble.jsx
@@ -2,22 +2,20 @@ import React, { useContext } from 'react'
 import { AuthContext } from '../../context/AuthContext'
 import './chatbubble.css'
 
-const ChatBubble = ({message, nextMessage, prevMessage, index}) => {
+const ChatBubble = ({message, isFirstMessage, isLastMessage, index}) => {
     const { loggedInUser } = useContext(AuthContext);
     const isUsersMessage = message.userSent._id === loggedInUser._id;
     const { firstName, lastName } = message.userSent;
 
-    const nextSender = nextMessage !== undefined ? nextMessage.userSent : ''; // If the next message exists, grabs the user from the message.
-    const prevSender = prevMessage !== undefined ? prevMessage.userSent : ''; // If the last sender doesnt equal the current messages sender, it must be the first message from a new user.
-    const firstMessage = prevSender._id !== message.userSent._id;
-    const lastMessage = nextSender._id !== message.userSent._id;
-
     return (
-        <li className={`message ${isUsersMessage ? 'isUsers' : 'isOthers'} ${firstMessage && 'firstMessage'} ${index === 0 && 'firstMessage'} ${lastMessage && 'lastMessage'}`}>
-            <div className='messageContainer' title={new Date(message.createdAt).toLocaleString('eu-US', {hour12: true})}>
-                <span className='messageBody'>{message.content}</span>
-            </div>
-        </li>
+        <>
+            {(!isUsersMessage && isFirstMessage) && <span className='senderName'>{firstName + " " + lastName}</span>}
+            <li className={`message ${isUsersMessage ? 'isUsers' : 'isOthers'} ${isFirstMessage && 'firstMessage'} ${index === 0 && 'firstMessage'} ${isLastMessage && 'lastMessage'}`}>
+                <div className='messageContainer' title={new Date(message.createdAt).toLocaleString('eu-US', {hour12: true})}>
+                    <span className='messageBody'>{message.content}</span>
+                </div>
+            </li>
+        </>
     )
 }
 
